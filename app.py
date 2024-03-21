5354d0e570504ee6a6345418d5194fd9106462b1
Ana Paula Badaró <140989367+quasiEvil@users.noreply.github.com>
2024-03-21 15:01:26 +0000

    aula do dia 21/03

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app  = Flask("Olá")
 
 
 @app.route("/")
-
 def ola():
-    return "Olá, mundo"
+    return "Olá, mundo!"
+
+@app.route("/alunos")
+def alunos():
+    return render_template("hello.html")
